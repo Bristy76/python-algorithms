1e1189eb9259d9e7e379cab37df9a38cc48203d3
Bristy <rimjhimbristy13579@gmail.com>
2023-06-23 23:55:03 +0600

    I have made some changes to files

--- common/test_find_max.py
+++ common/test_find_max.py
@@ -1,9 +1,18 @@
-from find_max import find_max
+#from find_max import find_max
 
 
-def test_find_max():
+def test_find_max(list1):
     """
     Checks if the number real max number in a list
     """
-    sample_list = [2, 0, 30, 55, 60, -66]
-    assert find_max(sample_list) == 60
+    max = list1[0]
+
+    for x in list1:
+        if x > max: # comparing the list
+          max = x
+
+    return max
+
+
+list1 = [2, 0, 30, 55, 60, -66]
+print("max number is:", test_find_max(list1))
